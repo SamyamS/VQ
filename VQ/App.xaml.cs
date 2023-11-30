a514a205098b43293a5a4f03b6e300fb5a72e1c9
PavanGuggilla <pavanguggilla1998@gmail.com>
2023-11-29 18:01:01 -0600

    Added Business Page

--- VQ/App.xaml.cs
+++ VQ/App.xaml.cs
@@ -1,9 +1,10 @@
-﻿namespace VQ {
+﻿using VQ.Pages;
+namespace VQ {
     public partial class App: Application {
         public App() {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new BusinessPage();
         }
     }
 }
